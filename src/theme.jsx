5dd44b97dd46b8b1b77eaac1612a78ea4df7ff05
Gokul Gopi <gokulg1412@gmail.com>
2022-08-06 20:48:18 +0530

    feat: ui for landing-page

---
+++ src/theme.jsx
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    primary: "#9fd3c7",
+    grey: "#979797",
+  },
+};
+
+export const theme = extendTheme({ colors });
